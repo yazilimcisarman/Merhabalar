68f5c9c4ea3fcb2d63d6494459a8b3bf9045ba67
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2024-04-04 11:36:16 +0300

    Merhabalar

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merhabalar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhabalaaaar");
+            Console.ReadLine();
+            Console.WriteLine("Sizlerle beraber kodalam pratikleri yapacağız");
+            Console.ReadLine();
+            Console.WriteLine("Evet enbaştan");
+            Console.ReadLine();
+            Console.WriteLine("Kısa yollar öğreneceğiz");
+            Console.ReadLine();
+            Console.WriteLine("Belki işinize yarar");
+            Console.ReadLine();
+            Console.WriteLine("Yeni gelecek videolardan haberdar olmak için");
+            Console.ReadLine();
+            Console.WriteLine("Abone olmayo unutmayııın :)");
+            Console.ReadLine();
+        }
+    }
+}
